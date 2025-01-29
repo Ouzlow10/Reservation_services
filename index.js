99d0b7c4c132838a2f2f0424d58f861e9e63cd7f
OUSMANE NDAO <ousmandao95@gmail.com>
2025-01-29 15:35:15 +0000

    make it better than version 5

--- index.js
+++ index.js
@@ -4,29 +4,46 @@ const firebase = require("firebase/app");
 const cookieParser = require('cookie-parser');
 var dotenv = require('dotenv');
 const {google} = require('googleapis');
+const crypto = require('crypto');
+const session = require('express-session');
+
+/**
+ * To use OAuth2 authentication, we need access to a CLIENT_ID, CLIENT_SECRET, AND REDIRECT_URI
+ * from the client_secret.json file. To get these credentials for your application, visit
+ * https://console.cloud.google.com/apis/credentials.
+ */
+const oauth2Client = new google.auth.OAuth2(
+  '1031708139330-3lce57ulejmb0fg674egm3bdhoo5gsh9.apps.googleusercontent.com',
+    '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',
+    'http://localhost:3000/oauth2callback'
+);
+
+// Access scopes for two non-Sign-In scopes: Read-only Drive activity and Google Calendar.
+const scopes = [
+  'https://www.googleapis.com/auth/drive.metadata.readonly',
+  'https://www.googleapis.com/auth/calendar.readonly'
+];
+
+// Generate a secure random state value.
+const state = crypto.randomBytes(32).toString('hex');
+
+// Store state in the session
+
+// Generate a url that asks permissions for the Drive activity and Google Calendar scope
+const authorizationUrl = oauth2Client.generateAuthUrl({
+  // 'online' (default) or 'offline' (gets refresh_token)
+  access_type: 'offline',
+  /** Pass in the scopes array defined above.
+    * Alternatively, if only one scope is needed, you can pass a scope URL as a string */
+  scope: scopes,
+  // Enable incremental authorization. Recommended as a best practice.
+  include_granted_scopes: true,
+  // Include the state parameter to reduce the risk of CSRF attacks.
+  state: state
+});
 
 dotenv.config();
 
-
-const oauth2Client = new google.auth.OAuth2(
-    '1031708139330-3lce57ulejmb0fg674egm3bdhoo5gsh9.apps.googleusercontent.com',
-    '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',
-    'http://localhost:3000/oauth2callback'
-  );
-  
-  // generate a url that asks permissions for Blogger and Google Calendar scopes
-  const scopes = [
-    'https://www.googleapis.com/auth/blogger',
-    'https://www.googleapis.com/auth/calendar'
-  ];
-  
-  const url = oauth2Client.generateAuthUrl({
-    // 'online' (default) or 'offline' (gets refresh_token)
-    access_type: 'offline',
-  
-    // If you only need one scope, you can pass it as a string
-    scope: scopes
-  });
 const PORT = process.env.PORT || 3000;
 const app = express();
 const http = require('http');
@@ -65,7 +82,25 @@ const firebaseConfig = {
     credential: admin.credential.cert(serviceAc),
   });
   const truncate = require('truncate-utf8-bytes');
+  const url = require('url');
 
+  // Receive the callback from Google's OAuth 2.0 server.
+  app.get('/oauth2callback', async (req, res) => {
+    let q = url.parse(req.url, true).query;
+    res.redirect(authorizationUrl);
+    req.session.state = state;
+
+  
+    if (q.error) { // An error response e.g. error=access_denied
+      console.log('Error:' + q.error);
+    } else if (q.state !== req.session.state) { //check state value
+      console.log('State mismatch. Possible CSRF attack');
+      res.end('State mismatch. Possible CSRF attack');
+    } else { // Get access and refresh tokens (if access_type is offline)
+  
+      let { tokens } = await oauth2Client.getToken(q.code);
+      oauth2Client.setCredentials(tokens);
+  }});
 // Each API may support multiple versions. With this sample, we're getting
 // v3 of the blogger API, and using an API key to authenticate.
 /*const blogger = google.blogger({
